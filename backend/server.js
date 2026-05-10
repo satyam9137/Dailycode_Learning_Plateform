42ce20cc064d248982edfaae0a0fd11e534e2196
satyam9137 <1satyamgupta123@gmail.com>
2026-05-10 23:42:59 +0530

    fixed express wildcard route

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.get("/health", (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
   });
 }
